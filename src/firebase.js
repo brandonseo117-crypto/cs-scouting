ff85769183de518c961d775d0c8c6217b9c0c2b4
Brandon Seo <brandonseo117@gmail.com>
2026-03-10 21:48:44 -0400

    move firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC7uerZ6ZUR4xv0NTZHIoXTulXSEidz6AE",
-    authDomain: "cs-scouting.firebaseapp.com",
-    projectId: "cs-scouting",
-    storageBucket: "cs-scouting.firebasestorage.app",
-    messagingSenderId: "912015511152",
-    appId: "1:912015511152:web:ed880a38178bf49fcaaeb5"
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
